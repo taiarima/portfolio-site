5f053b81ed07ea30ba642c1ee0a037297daae30a
taiarima <tai.arima@colorado.edu>
2023-03-25 03:21:15 +0200

    Working through initial stages of site creation.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <NavBar />
+      <Home />
     </div>
   );
 }
