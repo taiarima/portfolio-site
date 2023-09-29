1d9ad7871ba1c2095ac44425508ab80106990f1a
taiarima <tai.arima@colorado.edu>
2023-09-30 00:41:01 +0300

    Attempting to fix padding.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -49,7 +49,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-px-[150]"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-[150px]"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
