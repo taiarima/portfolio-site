846be16f9bb7d37f8df5cc7f169530f845cfe01c
taiarima <tai.arima@colorado.edu>
2023-09-30 00:55:20 +0300

    Attempting to fix Safari overlap.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -49,7 +49,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-[150px] flex-shrink-0"
+      className="bg-gradient-to-b from-gray-800 to-black w-full min-h-min pt-[150px]"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
